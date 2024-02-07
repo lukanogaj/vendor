3c924810c2346f8eb1f5f041e343908f868909e0
Lukasz Nogaj <lukasznogaj@Lukaszs-MacBook-Pro.local>
2024-02-07 20:42:12 +0000

    array-structure

--- src/components/VendorCard/index.jsx
+++ src/components/VendorCard/index.jsx
@@ -5,6 +5,42 @@ import { useState } from "react";
 
 const VendorCard = () => {
 	const [isOpen, setIsOpen] = useState(false);
+	// USe States for filter method
+	// const [filteredVendors, setFilteredVendors] = useState(dataForVendors);
+	// const [vendorId, setVendorId] = useState("");
+	// const [name, setName] = useState("");
+	// const [group, setGroup] = useState("");
+
+	/// Function to create unique values array
+	const uniqueVendors = dataForVendors.filter(function (item, index) {
+		return (
+			index ===
+			dataForVendors.findIndex(function (obj) {
+				return item.name === obj.name;
+			})
+		);
+	});
+	// Function to group the obejct with same names
+
+	/// Array of objects nested structure tree
+	const groupedVendors = [];
+	for (const uniq of uniqueVendors) {
+		const structure = {
+			vendor_id: uniq.id,
+			vendor_name: uniq.name,
+			group: [],
+		};
+		dataForVendors.forEach((v) => {
+			if (v.name === uniq.name) {
+				structure.group.push(v);
+			}
+		});
+
+		groupedVendors.push(structure);
+	}
+	console.log(groupedVendors);
+
+	// Function to show div with input and details
 	const handleCard = () => {
 		console.log("Clicked");
 		setIsOpen(!isOpen);
