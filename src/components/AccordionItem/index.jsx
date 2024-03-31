686f7351a1d5bed901275e85f3d014f370e2fa00
Lukasz Nogaj <lukasznogaj@Lukaszs-MacBook-Pro.local>
2024-03-31 14:03:05 +0100

    accordion-item

--- src/components/AccordionItem/index.jsx
+++ src/components/AccordionItem/index.jsx
@@ -2,25 +2,25 @@ import styles from "./index.module.scss";
 // import { useState } from "react";
 // import dataForVendors from "../../VendorDataArray";
 
-const AccordionItem = ({ activeIndex }) => {
-	return (
-		<div
-			className={
-				!activeIndex ? `${styles.accordionItem} ${styles.animated}` : ""
-			}
-		>
-			<label htmlFor="myInput">Name</label>
-			<input type="text" />
-			<label htmlFor="">Category</label>
-			<input type="text" />
-			<label htmlFor="">Address</label>
-			<input type="text" />
-			<label htmlFor="">Latitude</label>
-			<input type="text" />
-			<label htmlFor="">Longitude</label>
-			<input type="text" />
-		</div>
-	);
-};
+// const AccordionItem = ({ activeIndex }) => {
+// 	return (
+// 		<div
+// 			className={
+// 				!activeIndex ? `${styles.accordionItem} ${styles.animated}` : ""
+// 			}
+// 		>
+// 			<label htmlFor="myInput">Name</label>
+// 			<input type="text" />
+// 			<label htmlFor="">Category</label>
+// 			<input type="text" />
+// 			<label htmlFor="">Address</label>
+// 			<input type="text" />
+// 			<label htmlFor="">Latitude</label>
+// 			<input type="text" />
+// 			<label htmlFor="">Longitude</label>
+// 			<input type="text" />
+// 		</div>
+// 	);
+// };
 
-export default AccordionItem;
+// export default AccordionItem;
